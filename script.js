039bfd5822bf5ccf8467814d4e0dce8a20b43039
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-04-20 14:38:45 +0530

    Modified getComputerChoice and playRound Function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const array=["rock","paper","scissors"]
-
-function getComputerChoice(array){
+function getComputerChoice(){
+    const array=["rock","paper","scissors"]
     const index=Math.floor(Math.random()*array.length);
     const ComputerChoice=array[index];
     return ComputerChoice;
@@ -10,11 +9,28 @@ function playRound(playerSelection, computerSelection) {
     if(playerSelection===computerSelection){
         console.log("Tie,Both Player entered the same sign")
     }
-    else if(playerSelection==="rock")
+    else if(playerSelection==="rock"){
         if(computerSelection==="paper"){
             console.log("Paper Won!!,You Lost")
         }
         else{
-            console.log("Rock Won!!,Computer Lost")
+            console.log("Rock Won!!,Computer Lost") 
         }
     }
+    else if(playerSelection==="paper"){
+        if(computerSelection==="rock"){
+            console.log("Paper Won!!,Computer Lost")
+        }
+        else{
+            console.log("Scissors Won!!,You Lost")
+        }
+    }
+    else if(playerSelection==="scissors"){
+        if(computerSelection==="rock"){
+            console.log("Rock Won!!,You Lost")
+        }
+        else{
+            console.log("Scissors Won!!,Computer Lost")
+        }
+    }
+}
