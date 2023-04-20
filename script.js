f0b418a1fafe99ac7aba147fd0a382169668cbc8
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-04-20 17:25:07 +0530

    Added semicolons

--- script.js
+++ script.js
@@ -2,42 +2,42 @@
 let computercounter=0;
 let playercounter=0;
 function getComputerChoice(){
-    const array=["rock","paper","scissors"]
+    const array=["rock","paper","scissors"];
     const index=Math.floor(Math.random()*array.length);
     const ComputerChoice=array[index];
     return ComputerChoice;
 }
 function playRound(playerSelection, ComputerChoice) {
     if(playerSelection===ComputerChoice){
-        alert("Tie,Both Player entered the same sign")
+        alert("Tie,Both Player entered the same sign");
     }
     else if(playerSelection==="rock"){
         if(ComputerChoice==="paper"){
-            alert("Paper Won!!,You Lost")
+            alert("Paper Won!!,You Lost");
             computercounter++;
         }
         else{
-            alert("Rock Won!!,Computer Lost")
+            alert("Rock Won!!,Computer Lost");
             playercounter++; 
         }
     }
     else if(playerSelection==="paper"){
         if(ComputerChoice==="rock"){
-            alert("Paper Won!!,Computer Lost")
+            alert("Paper Won!!,Computer Lost");
             playercounter++;
         }
         else{
-            alert("Scissors Won!!,You Lost")
+            alert("Scissors Won!!,You Lost");
             computercounter++;
         }
     }
     else if(playerSelection==="scissors"){
         if(ComputerChoice==="rock"){
-            alert("Rock Won!!,You Lost")
+            alert("Rock Won!!,You Lost");
             computercounter++;
         }
         else{
-            alert("Scissors Won!!,Computer Lost")
+            alert("Scissors Won!!,Computer Lost");
             playercounter++;
         }
     }
@@ -52,20 +52,20 @@ function game(){
         let ComputerChoice=getComputerChoice();
         let playerSelection=getplayerSelection();
         if(playerSelection==="rock"||playerSelection==="scissors"||playerSelection==="paper"){
-            playRound(playerSelection, ComputerChoice)
+            playRound(playerSelection, ComputerChoice);
         }
         else{
-            alert("Please Enter a Valid Choice")
+            alert("Please Enter a Valid Choice");
         }
     }
     if(playercounter>computercounter){
-        alert("Congratulation!!,You have won the game with ${playercounter - computercounter} points")
+        alert("Congratulation!!,You have won the game with ${playercounter - computercounter} points");
     }
     else if(playercounter==computercounter){
-        alert("The game ended in a tie,Please Refresh to play again")
+        alert("The game ended in a tie,Please Refresh to play again");
     }
     else{
-        alert("Game Lost,Please Refresh to play again")
+        alert("Game Lost,Please Refresh to play again");
     }
 }
 game()  
