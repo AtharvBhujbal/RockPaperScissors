7925763e226c2f43286dd5d97bad69a16c7235d8
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-04-25 13:10:08 +0530

    it is a test

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function playRound(playerSelection, ComputerChoice) {
         }
     }
 }
+// This is a random comment
 function getplayerSelection(){
     let playerSelection=prompt("Please Enter Your choice from Rock,Paper,Scissors");
     playerSelection=playerSelection.toLowerCase();
